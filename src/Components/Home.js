653b0d5738b22a8de9703deb70be42b61b1ff088
yogesh <hemant.kaushik@writlabs.com>
2023-06-09 13:08:20 +0530

    clock

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 function Home() {
+  function Clock({ time }) {
+    let hours = time.getHours();
+    let className;
+    if (hours >= 0 && hours <= 6) {
+      className = "night";
+    } else {
+      className = "day";
+    }
+    return <h1 className={className}>{time.toLocaleTimeString()}</h1>;
+  }
+
+  function useTime() {
+    const [time, setTime] = useState(() => new Date());
+    useEffect(() => {
+      const id = setInterval(() => {
+        setTime(new Date());
+      }, 1000);
+      return () => clearInterval(id);
+    }, []);
+    return time;
+  }
+  const time = useTime();
   return (
     <div
       style={{
@@ -27,9 +49,13 @@ function Home() {
           display: "flex",
           height: "auto",
           justifyContent: "flex-end",
+          flexDirection: "column",
+          alignItems: "end",
         }}
       >
         <Calendar />
+        <Clock time={time} />
+
         <br />
       </div>
     </div>
